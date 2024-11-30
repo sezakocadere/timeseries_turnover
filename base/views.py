6097b624ee80cac73709b5b44288a9b9fdc49818
sezakocadere <seza.kocadere@gmail.com>
2024-11-30 11:43:55 +0100

    get uploaded dataset and print status message

--- base/views.py
+++ base/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import os
+import pandas as pd
+from django.conf import settings
+
 def home(request):
+    return render(request, "home.html")
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        uploaded_file = request.FILES['file']
+        print("file uploaded")
+
+        file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', uploaded_file.name)
+
+        with open(file_path, 'wb') as f:
+            for chunk in uploaded_file.chunks():
+                f.write(chunk)
+        if uploaded_file.name.endswith('.feather'):
+            data = pd.read_feather(file_path)
+        print(data)
+
+        return render(request, "home.html")
     return render(request, "home.html")
--- timeseries_turnover/urls.py
+++ timeseries_turnover/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from base import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("base.urls"))
+    path("", include("base.urls")),
+    path("upload/", views.upload_file, name='upload-file'),
 ]
