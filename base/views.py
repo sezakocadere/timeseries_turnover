90bdb9bf8f0fdfeaa3ccab5ed60fff8a9446df1c
sezakocadere <seza.kocadere@gmail.com>
2024-12-02 15:07:21 +0100

    edit files and add map marks

--- base/views.py
+++ base/views.py
@@ -4,26 +4,38 @@ from django.http import HttpResponse
 import os
 import pandas as pd
 from django.conf import settings
+import json
+from django.shortcuts import render, redirect
+
 
 def home(request):
     return render(request, "home.html")
+
+def save_markers_to_json(data, json_file_path):
+    markers = data[['latitude', 'longitude']].values.tolist()
+
+    with open(json_file_path, 'w') as f:
+        json.dump(markers, f)
 
 def upload_file(request):
     if request.method == 'POST' and request.FILES.get('file'):
         uploaded_file = request.FILES['file']
         print("file uploaded")
 
-        file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', uploaded_file.name)
+        file_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
 
         with open(file_path, 'wb') as f:
             for chunk in uploaded_file.chunks():
                 f.write(chunk)
-       
+
+        data = pd.read_feather(file_path)
+
+        json_file_path = os.path.join(settings.MEDIA_ROOT, 'markers.json')
+        save_markers_to_json(data, json_file_path)
+
 
         return render(request, "home.html")
     return render(request, "home.html")
 
-#def get_locations(request):
-#      uploads_dir = os.path.join(settings.MEDIA_ROOT, 'uploads')
-#     file_path = os.path.join(uploads_dir, filename)
-    
+
+ 
--- timeseries_turnover/urls.py
+++ timeseries_turnover/urls.py
@@ -19,6 +19,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from base import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path("", include("base.urls")),
     path("upload/", views.upload_file, name='upload-file'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
